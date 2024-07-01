18246bbea7eff25d2de5e8af231e21b46a7cd653
Sergei Kuklin <sergei.kuklin@forto.com>
2024-07-02 00:05:02 +0200

    Fix edit workouts

--- src/pages/EditWorkoutPage.tsx
+++ src/pages/EditWorkoutPage.tsx
@@ -6,12 +6,8 @@ import { Page } from 'shared/ui/Page';
 export const EditWorkoutPage = () => {
   const workout = useLoaderData() as Workout;
 
-  if (!workout) {
-    return <div>Loading...</div>;
-  }
-
   return (
-    <Page title="Edit Workouts">
+    <Page title="Edit Workout">
       <EditWorkout workout={workout} />
     </Page>
   );
