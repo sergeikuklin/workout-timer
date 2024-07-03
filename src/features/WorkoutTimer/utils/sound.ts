0b6256ba2eaa3de041eb24fe1ba6dc15d9c1c99b
Sergei Kuklin <sergei.kuklin@forto.com>
2024-07-04 00:07:38 +0200

    Update sound

--- src/features/WorkoutTimer/utils/sound.ts
+++ src/features/WorkoutTimer/utils/sound.ts
@@ -1,6 +1,6 @@
-const secondSound = new Audio('/public/1sec.mp3');
-const lastSecondSound = new Audio('/public/last.mp3');
-const completeSound = new Audio('/public/complete.mp3');
+const secondSound = new Audio('public/1sec.mp3');
+const lastSecondSound = new Audio('public/last.mp3');
+const completeSound = new Audio('public/complete.mp3');
 
 const playSound = (sound: HTMLAudioElement) => {
   sound.pause();
