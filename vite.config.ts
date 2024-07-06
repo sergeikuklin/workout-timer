172400bfc54cb1416a27f371e9a622ff5481a501
Sergei Kuklin <sergei.kuklin@forto.com>
2024-07-07 00:08:41 +0200

    Cache mp3

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,9 @@ export default defineConfig({
       devOptions: {
         enabled: true,
       },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,mp3}'],
+      },
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
       manifest: {
         name: 'Workout Timer',
