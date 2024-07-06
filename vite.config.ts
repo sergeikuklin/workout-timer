6e240c66fdbe65f6b4f2518a125115ad78bc4240
Sergei Kuklin <sergei.kuklin@forto.com>
2024-07-06 23:56:19 +0200

    Add pwa assets

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,36 @@ export default defineConfig({
       devOptions: {
         enabled: true,
       },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Workout Timer',
+        short_name: 'WorkoutTimer',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
     }),
   ],
   base: process.env.NODE_ENV === 'production' ? '/workout-timer/' : '/',
