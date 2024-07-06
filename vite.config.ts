ddda1f8c9b116b497f764bb3931befb6be926a2b
Sergei Kuklin <sergei.kuklin@forto.com>
2024-07-06 23:28:22 +0200

    Enable pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
   base: process.env.NODE_ENV === 'production' ? '/workout-timer/' : '/',
   resolve: {
     alias: {
